4ae987b587a4d5b35ba7a0ac41e58de2dd0c40b5
shishkina <shishkina.kate@gmail.com>
2016-04-06 16:04:51 -0400

    added projects, still need revision

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ var app = express();
 var port = process.env.PORT
 app.use(express.static('public'));
 
-app.listen(port);
+//app.listen(port);
+app.listen(3000, function(){
+  console.log('Listening on port 3000..');
+});
