31fc81a8cb976db2ce61cb23cb9259c91e3bfc7e
shishkina <shishkina.kate@gmail.com>
2016-04-22 10:20:16 -0400

    added serve-favicon to ndeploy on heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT
+var port = process.env.PORT;
+var favicon = require('serve-favicon');
 app.use(express.static('public'));
 
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 //app.listen(port);
 app.listen(3000, function(){
   console.log('Listening on port 3000..');
