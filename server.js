9b7175d1a67dac7194ac03075e92f5cf74b04b63
shishkina <shishkina.kate@gmail.com>
2016-04-25 13:08:44 -0400

    modified port to fix the heroku errors

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT;
 var favicon = require('serve-favicon');
 app.use(express.static('public'));
 
 app.use(favicon(__dirname + '/public/img/favicon.ico'));
-//app.listen(port);
-app.listen(3000, function(){
-  console.log('Listening on port 3000..');
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port 3000...");
 });
