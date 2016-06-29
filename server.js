c2117f14e01e4f7f7db00f7e07e5d204abe5491e
shishkina <shishkina.kate@gmail.com>
2016-06-29 15:21:52 -0400

    changed size of images, minor modifications to styling

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port 3000...");
 });
+
+// app.get('/img/:name', function(req, res, next){
+//     var options = {
+//     root: __dirname + '/public/',
+//     dotfiles: 'deny',
+//     headers: {
+//         'x-timestamp': Date.now(),
+//         'x-sent': true
+//       }
+//     };
+// });
